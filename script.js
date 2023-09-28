176297a5fc5980341d816b2839cff7bbbdca9cfe
Jair-pi <104595899+Jair-pi@users.noreply.github.com>
2023-09-28 11:15:37 -0500

    se agrega el proyecto

---
+++ script.js
@@ -0,0 +1,547 @@
+var coco_song = `Ante que este Phillie se apague, yo te voy a olvidar-dar
+Yo te voy a olvidar-dar
+Ante que este Phillie se apague, yo te voy a olvidar-dar-dar
+Yo te voy a olvidar, yal
+Me paso fumando bajo las palmera
+A ver si me cae un coco en la cabeza y me borra mi vida entera
+Veo parejitas dándose beso y me da dentera
+Vo'a tirar el radio pa'l mar si sale Sin Bandera porque
+No puedo escuchar cancione dе romance (du)
+Y me cansé que dе pensar en ti no me canse
+El último pétalo me dijo que aún hay chance
+No sé si e verdad, pero si va a volver, avance
+Por favor, aunque sea un mensaje ante que salga el sol
+Que en la mañana coja alergia buscando tu olor
+Tantos trofeo y aún me siento perdedor, perdedor
+Búscame el encendedor, ey
+Que si no te olvido me vo'a mudar pa Chile
+Pa Argentina o pa El Salvador
+"Pero demen el lighter" dije, por favor, por favor
+Y ante que este Phillie se apague, yo te voy a olvidar
+Ey, yo te voy a olvidar-dar
+Ante que este Phillie se apague, yo te voy a olvidar-dar (ey)
+Yo te voy a olvidar, yal
+Ey, ey, ey
+Me estoy ahogando y a la ola le pedí socorro
+Escribo tu nombre en la arena a ver si así lo borro
+Pero tus labio y tus ojo los tengo de gorro
+No sale de mi cabeza (du)
+Otro Phillie y otra cerveza
+Ya tu recuerdo me pesa
+Te disfrazaste de Rubí y eras Teresa
+Pero soy un pendejo y todavía me interesa
+Ey, ey, saber de tu paradero
+Y preguntarte si fue verdadero
+O si está volá como avión y yo fui pasajero (wuh)
+Qué cojones, todavía yo te quiero
+Jeh, jeh, lo muchacho dicen que yo estoy charriando
+Jeh, jeh, ey, pero en bajita seguimo quemando (ra)
+Y ante que este Phillie se apague, yo te voy a olvidar-dar (eh)
+Yo te voy a olvidar-dar
+Ante que este Phillie se apague, yo te voy a olvidar-dar
+Yo te voy a olvidar, yal
+Y ante que este Phillie se apague, yo te voy a olvidar-dar
+Ey, yo te voy a olvidar-dar
+Y ante que este Phillie se apague, yo te voy a olvidar-dar
+Ey, yo te voy a olvidar, yal`;
+
+function info_coco() {
+
+coco_song = coco_song.replace(/\n/gi,"<br>")
+console.log(coco_song)
+document.getElementsByClassName("song-name")[0].innerHTML = "Un coco - Bad Bunny";
+document.getElementsByClassName("lyrics")[0].innerHTML = coco_song.slice(0,50)+"...";
+document.getElementsByClassName("duration-words")[0].innerHTML = "Duración: 3:16\ - Albun: Un Verano Sin Ti";
+
+}
+
+info_coco();
+
+var titi_song = `Ey, Tití me preguntó
+Si tengo muchas novia'
+Muchas novia'
+Hoy tengo a una, mañana otra
+Ey, pero no hay boda
+Tití me preguntó
+Si tengo muchas novia'
+Je, muchas novia'
+Hoy tengo una, mañana otra
+Me la' vo'a llevar a to'a
+Pa' un VIP, un VIP, ey
+Saluden a Tití
+Vamo' a tirarno' un selfie
+Say "Cheese", ey
+Que sonrían las que ya les metí
+En un VIP, un VIP, ey
+Saluden a Tití
+Vamo' a tirarno' un selfie
+Say "Cheese"
+Que sonrían las que ya se olvidaron de mí
+Me gustan mucho las Gabriela
+Las Patricia, las Nicolle, las Sofía
+Mi primera novia en kinder, María
+Y mi primer amor se llamaba Thalía
+Tengo una colombiana
+Que me escribe to' los día'
+Y una mexicana que ni yo sabía
+Otra en San Antonio que me quiere todavía
+Y las de PR que todita' son mía'
+Una dominicana que es uva bombón
+Uva, uva bombón
+La de Barcelona que vino en avión
+Y dice que mi bicho está cabrón
+Yo dejo que jueguen
+Con mi corazón
+Quisiera mudarme
+Con todas pa' una mansión
+El día que me case te envío la invitación
+Muchacho, deja eso, ey
+Tití me preguntó
+Si tengo muchas novia', muchas novia'
+Hoy tengo una, mañana otra
+Ey, pero no hay boda
+Tití me preguntó
+Si tengo muchas novia'
+Ey, ey, muchas novia'
+Hoy tengo una, mañana otra (mañana otra) (rrra)
+Tití me preguntó-tó-tó-tó-tó-tó-tó
+Tití me preguntó-tó-tó-tó-tó-tó-tó (qué pámpara)
+Tití me preguntó-tó-tó-tó-tó-tó-tó
+Tití me preguntó-tó-tó-tó
+(Pero ven acá, muchacho
+¿Y para qué tú quiere' tanta' novia'?)
+Me la' vo'a llevar a to'a
+Pa' un VIP, un VIP, ey
+Saluden a Tití
+Vamo' a tirarno' un selfie
+Say "Cheese", ey
+Que sonrían las que ya les metí
+En un VIP, un VIP, ey
+Saluden a Tití
+Vamo' a tirarno' un selfie
+Say "Cheese"
+Que sonrían las que ya se olvidaron de mí
+Oye, muchacho 'el diablo, azaroso
+Suelta ese mal vivir
+Que tú tiene' en la calle
+Búscate una mujer seria pa' ti
+Muchacho 'el diablo, coño
+Yo quisiera enamorarme
+Pero no puedo
+Pero no puedo, eh, eh
+Yo quisiera enamorarme
+Pero no puedo
+Pero no puedo
+Sorry, yo no confío, yo no confío
+Nah, ni en mí mismo confío
+Si quieres quedarte
+Hoy que hace frío
+Y mañana te va', nah
+Muchas quieren mi baby gravy
+Quieren tener mi primogénito, ey
+Y llevarse el crédito
+Ya me aburrí
+Hoy quiero un totito inédito, je
+Uno nuevo, uno nuevo, uno nuevo, uno nuevo (ey)
+Hazle caso a tu amiga
+Ella tiene razón
+Yo vo'a romperte el corazón
+Vo'a romperte el corazón
+Ey, no te enamores de mí
+No te enamores de mí
+Sorry, yo soy así, ey
+No sé por qué soy así
+Hazle caso a tu amiga
+Ella tiene razón
+Yo vo'a romperte el corazón
+Vo'a romperte el corazón
+No te enamores de mí
+No te enamores de mí
+Sorry, yo soy así
+Ya no quiero ser así, no`;
+
+function info_titi() {
+
+titi_song = titi_song.replace(/\n/gi,"<br>")
+console.log(titi_song)
+document.getElementsByClassName("song-name")[1].innerHTML = "Titi Me Preguntó";
+document.getElementsByClassName("lyrics")[1].innerHTML = titi_song.slice(0,50)+"...";
+document.getElementsByClassName("duration-words")[1].innerHTML = "Duración: 4:03\ - Albun: Un Verano Sin Ti";
+
+}
+
+info_titi();
+
+
+var ojitos_song = `Hace mucho tiempo le hago caso al corazón
+Y pasan los día', los mese' pensando en tu olor (en tu olor)
+Ha llegado el tiempo para usar la razón
+Antes que sea tarde y sin querer me parta en do' (me parta en do')
+Antes de que salga el sol, hunde el acelerador
+Aunque vaya sin freno' y pierda el control
+Nada más seremos do', tú y yo acariciándono'
+En medio del tiempo, sin decir "adiós"
+Y solo mírame con esos ojitos lindos
+Y con eso yo estoy bien
+Hoy he vuelto a nacer
+Hacе tiempo que no agarro a nadie de la mano
+Hace tiempo que no envio, "buenos días, te amo"
+Pero tú me tienes enredao
+Me envolví
+Iba por mi camino y me perdí
+Mi mirada cambió cuando tus ojos ví
+Bye-bye a los culo', ni me despedí
+Yo no te busqué, no, chocamo' en el trayecto
+Con tu alma es la que yo conecto
+Tranquila, no tiene que ser perfecto, no
+Aquí no existe el pecado y equivocarse es bonito
+Los errores son placeres igual que to' tus besito'
+Y solo mírame con esos ojitos lindos
+Que con eso yo estoy bien
+Hoy he vuelto a nacer
+Y solo mírame con esos ojitos lindos
+Que con eso yo estoy bien
+Hoy he vuelto a nacer
+Tú y yo
+Tú y yo
+Tú y yo
+Tú y yo (tú y yo)
+Tú y yo
+Tú y yo (tú y yo)
+Tú y yo (tú y yo)
+Tú y yo, tú y yo
+Tú y yo, tú y yo
+Yo no me dejo llevar de nadie
+Yo solo me dejo llevar de tu sonrisa
+Y del lunar cerquita de tu boca
+Si yo 'toy loco, tú estás loquita
+Pero baby, como tú no hay otra, no
+Quiero regalarte girasole'
+Ir pa' la playa y buscarte caracole'
+Cuando estoy contigo, yo no miro el Role'
+Vamo' a bailar doscientas cancione'
+Nadie me pone como tú me pone'
+Mm-mmm, mmm-mmm, mmm-mmm
+Mm-mmm, mmm-mmm, mmm-mmm
+Yo le hablo a Dio' y tú ere' su respuesta
+Aprendí que los momento' lindo' nunca cuestan
+Como cuando me regalas tu mirada
+Y el sol su puesta, (ey, ey) y el sol su puesta
+Cuando estoy encima de ti, de ti
+Mami, yo me olvido de to', de to'
+No hace falta nadie aquí
+Solamente tú y yo
+Antes de que salga el sol, hunde el acelerador
+Aunque vaya sin freno' y pierda el control
+Nada más seremos do', tú y yo acariciándono'
+En medio del tiempo, sin decir "adiós"
+Y solo mírame con esos ojitos lindos
+Que con eso yo estoy bien
+Hoy he vuelto a nacer
+Y solo mírame con esos ojitos lindos
+Que con eso yo estoy bien
+Hoy he vuelto a nacer`;
+function info_ojitos() {
+
+ojitos_song = ojitos_song.replace(/\n/gi,"<br>")
+console.log(ojitos_song)
+document.getElementsByClassName("song-name")[2].innerHTML = "Ojitos Lindos";
+document.getElementsByClassName("lyrics")[2].innerHTML = ojitos_song.slice(0,50)+"...";
+document.getElementsByClassName("duration-words")[2].innerHTML = "Duración: 4:18\ - Albún: Un Verano Sin Ti";
+
+}
+
+info_ojitos();
+
+var ratito_song = `Yeah-yeah-yeah-yeah
+Yeah-yeah-yeah-yeah, yeah
+Ey
+Quizás yo vuelva con mi ex
+Y no te vuelva a ver (y no te vuelva a ver)
+Quizá no te importa
+Quizá te va a doler
+Esto e' un ratito, uoh
+Mami, no te acostumbre', eh
+Que el amor es muy bonito
+Pero siempre hay algo que lo interrumpe, eh
+Esto e' un ratito, uoh
+Mami, no te acostumbre', eh
+Que el amor es muy bonito
+Pero siempre hay algo que lo interrumpe, eh
+Pa' mí que yo nací pa' estar solo
+No hay una loca pa' este loco
+El tiempo se va y queda poco
+Baby, vamo' a hacerlo otra ve'
+A chingar otra ve'
+Porque mañana quizá no vo'a estar
+La luz se puso roja, hay que parar
+O si quieres nos bajamo' y podemos perrear
+Baby, esto no fue normal
+Con cualquiera no me acuesto, a cualquiera no le meto
+Ni le cuento mis secreto'
+Me pongo feliz cuando llegan tus texto' porque
+Con cualquiera no me acuesto, a cualquiera no le meto
+Ni le cuento mis secreto'
+Me pongo feliz cuando llegan tus texto'
+O cuando en cuatro te lo meto
+Ey, y te moja to'a
+Contigo me voy a to'a, ey
+Espero que lo hayas pasa'o bien
+Y en verdad, tú eres bien pero
+Mi vida es complicada, tú no va' a entender
+Un poco inestable como puedes ver (puedes ver)
+Mami, fue rico conocerte
+Pero no sé si vuelva a verte
+Esto e' un ratito, uoh
+Mami, no te acostumbre', eh
+Que el amor es muy bonito
+Pero siempre hay algo que lo interrumpe, eh
+Pa' mí que yo nací pa' estar solo (solo)
+Pa' mí que yo nací pa' estar solo`;
+
+function info_ratito() {
+
+ratito_song = ratito_song.replace(/\n/gi,"<br>")
+console.log(ratito_song)
+document.getElementsByClassName("song-name")[3].innerHTML = "Un Ratito";
+document.getElementsByClassName("lyrics")[3].innerHTML = ratito_song.slice(0,50)+"...";
+document.getElementsByClassName("duration-words")[3].innerHTML = "Duración: 2:56\ - Albún: Un Verano Sin Ti";
+
+}
+
+info_ratito();
+
+var efecto_song = `Yeah-yeah-yeah-yeah-yeah-yeah
+Eh-eh-eh-eh
+No sé si es casualidad que yo me sienta así
+Siempre que tú estás cerquita de mí
+Dime qué me hiciste, qué droga me diste
+Que desde aquella noche no soy igual
+Tú me mira' y empiezo a sudar
+Siento que puedo volar
+Baby, la nota ya está haciendo efecto
+Mi mundo está jodío y me siento perfecto
+Porque estás tú aquí, moviéndote así, no pare'
+Baby, tú eres mi droga, esta noche no le baje'
+Porque la nota ya está haciendo efecto
+Mi mundo está jodío y me siento perfecto
+Porque estás tú aquí, moviéndote así, no pare'
+Baby, tú eres mi droga, esta noche no le baje'
+Esta no, esta sí
+Esta no, esta sí
+Esta no, esta sí
+Esta no, esta sí
+El gatito tuyo te perdió por negligencia
+Y yo que no creo en la abstinencia
+Esta noche en la cama va a haber turbulencia
+Qué rico tú mama', te vo'a dar la permanencia
+Ese totito e' la eminencia
+Pa darle tengo licencia
+Desde que fuimo' a Florencia
+Se puso más bicha, pero no pierde la esencia
+No, no, de Carola
+No, no, no, no anda sola
+No, no, no le diga' hola
+O va' a ser otro pa la cola, je
+Tu crico te me amola'
+Yo soy fan de esa popola
+Más las picky, las endo, la coca y la rola (más nada)
+Eres tú quien me controla, eh (je, je)
+En tus ojo' veo el mar, mami, llévame en tu ola
+Hoy me siento bien puta, re piola, ey
+Porque la nota ya está haciendo efecto
+Mi mundo está jodío y me siento perfecto
+Porque estás tú aquí, moviéndote así, no pare'
+Baby, tú ere mi droga, esta noche no le baje'`;
+
+function info_efecto() {
+
+efecto_song = efecto_song.replace(/\n/gi,"<br>")
+console.log(efecto_song)
+document.getElementsByClassName("song-name")[4].innerHTML = "Efecto";
+document.getElementsByClassName("lyrics")[4].innerHTML = efecto_song.slice(0,50)+"...";
+document.getElementsByClassName("duration-words")[4].innerHTML = "Duración: 3:33\ - Albún: Un Verano Sin Ti";
+
+}
+
+info_efecto();
+
+var meportobonito_song = `Yeah-yeah-yeah-yeah-yeah-yeah, ey
+En la guagua se quedó el olor
+De tu perfume
+Tú ere' una bellaca, yo soy un bellaco
+Eso e' lo que no' une
+Ella sabe que está buenota
+Y no la presumen
+Si yo fuera tu gato
+Subiera una foto los vierne' y los lune' (so)
+Pa' que to' el mundo vea
+Lo rica que tú está', que tú está'
+Contigo tengo que apretar
+Y en la calle ando suelto
+Pero por ti me quito
+Si tú me lo pides
+Yo me porto bonito
+Y subo un selfie pa' que vean
+Lo rica que tú está', que tú está'
+Las otras tienen que apretar
+En la calle ando suelto
+Pero por ti me quito
+Si tú me lo pide'
+Yo me porto bonito
+Ey, ey
+Tú no ere' bebecita
+Tú ere' bebesota
+Frikitona, ma', se te nota
+Le gustan los trío'
+Cuando está en la nota
+Si el novio no sirve
+De una lo bota
+Y waoh, mami
+Dime dónde dejo el resume
+Sé que te dejaste hace un me'
+Y de una de pecho me zumbé
+Si quiere' te hago un bebé
+O te traigo la' Plan B
+Uff, mami, qué rica tú te ve'
+Pa' los 2000 escuchaba RBD
+Y ahora quiere perreo
+Toa' la noche en la pared
+Te obsesiono, CBD
+Mami, tú ere' élite
+No te me limite'
+Déjame hacerte
+Lo que amerite y te levite
+Dale pa'l escondite
+No te me arisque'
+Que aquí no van a poder llegar lo' satélite'
+Mami, sube algo
+Dame contenido
+Ese culo súbelo má' seguido
+Me paso jangueando
+A ver si coincido, ey
+Y por fin doy contigo
+Mai, nuestra' bellaquera'
+Nunca la' olvido
+Ninguna como tú
+A mí me ha complacido
+Tú cree' que a toda'
+Siempre les digo lo mismo
+Quiere chingar
+Pero no quiere na' fijo, ey
+Pero en confianza me confiesa
+Dice que nadie le interesa
+Pero cuando sale, se pone traviesa (ey)
+Pa' que to' el mundo vea
+Lo rica que tú está', que tú está', ey
+Contigo tengo que apretar
+Y en la calle ando suelto
+Pero por ti me quito
+Si tú me lo pide'
+Yo me porto bonito`;
+
+function info_meportobonito() {
+
+  meportobonito_song = meportobonito_song.replace(/\n/gi,"<br>")
+  console.log(meportobonito_song)
+  document.getElementsByClassName("song-name")[5].innerHTML = "Me Porto Bonito";
+  document.getElementsByClassName("lyrics")[5].innerHTML = meportobonito_song.slice(0,50)+"...";
+  document.getElementsByClassName("duration-words")[5].innerHTML = "Duración: 3:43\ - Albún: Un Verano Sin Ti";
+  
+}
+  
+  info_meportobonito();
+
+  var vacaciones_song = `Ey, ey
+  El sol salió a janguear con las nube'
+  Y me regalaron un día cabrón
+  Como cuando me quedaba en Boquerón
+  O en Manatí
+  Lo mejor nunca se sube
+  Al menos que en la radio salga esta canción
+  Hoy vo'a darle paz a mi corazón
+  Por eso me perdí
+  Me fui de vacacione'
+  Con muchas cerveza' y cancione'
+  Un shot
+  Por las buenas amistade'
+  Y por las bendicione'
+  Y si el día se pone feo
+  Tú me lo haces bonito
+  Ya no pido más deseo'
+  Tengo todo lo que necesito
+  Ey, ey
+  Mi shorty con la gente
+  Que quiere saberlo to'
+  Si pongo el location
+  Ya no e' un secret spot
+  Hoy vo'a pasarla bien
+  Y no pensar en na'
+  Y no pensar en na'
+  Y no pensar en na'
+  Porque a veces pienso tanto
+  Que me olvido de dar gracias cuando me levanto
+  A veces pienso tanto
+  Que me olvido de pensar en mí
+  Que me olvido hasta de dormir, ey
+  Así que no se asusten si desaparezco
+  Un rato bien lejo', yo me lo merezco
+  No me hablen del trabajo,
+  No me hablen de escuela
+  Que ya subí el ancla y abrí las vela'
+  Puedo ver el paraíso llegando a Isabela
+  Un mango de Mayagüe' y despué' pa' La Palguera
+  Puerto Rico se ve lindo hasta en Google Maps
+  Y hoy le quiero dar la vuelta con Sensei, con Gabs
+  Lo que traigo es vibra linda
+  No quiero frontear
+  Pa' la' babie' yo después
+  Saco un disco de trap
+  No me busque'
+  Que no me va' a encontrar
+  No me va' a encontrar, no, no
+  Me fui de vacacione'
+  Con muchas cerveza' y cancione'
+  Un shot
+  Por las buenas amistade'
+  Y por las bendicione'
+  Y si el día se pone feo
+  Tú me lo haces bonito
+  Ya no pido más deseo'
+  Tengo todo lo que necesito
+  Ey, ya no pido más deseo'
+  Tengo todo lo que necesito`
+
+  function info_vacaciones() {
+
+    vacaciones_song = vacaciones_song.replace(/\n/gi,"<br>")
+    console.log(vacaciones_song)
+    document.getElementsByClassName("song-name")[6].innerHTML = "Me Fui De Vacaciones";
+    document.getElementsByClassName("lyrics")[6].innerHTML = vacaciones_song.slice(0,50)+"...";
+    document.getElementsByClassName("duration-words")[6].innerHTML = "Duración: 3:00\ - Albún: Un Verano Sin Ti";
+    
+  }
+    
+    info_vacaciones();
+
+function coco() {
+  document.getElementsByClassName("lyrics")[0].innerHTML = coco_song
+}
+function Titi() {
+  document.getElementsByClassName("lyrics")[1].innerHTML = titi_song
+}
+function ojitos() {
+  document.getElementsByClassName("lyrics")[2].innerHTML = ojitos_song
+}
+function ratito() {
+  document.getElementsByClassName("lyrics")[3].innerHTML = ratito_song
+}
+function efecto() {
+  document.getElementsByClassName("lyrics")[4].innerHTML = efecto_song
+}
+function meportobonito() {
+  document.getElementsByClassName("lyrics")[5].innerHTML = meportobonito_song
+}
+function Vacaciones() {
+  document.getElementsByClassName("lyrics")[6].innerHTML = vacaciones_song
+}
